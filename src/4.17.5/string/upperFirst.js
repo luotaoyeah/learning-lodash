630246175cf0ac32479b235240665da1e5b3e4b3
luotao <luotaoyeah@gmail.com>
2018-04-08 14:02:16 +0800

    string: upperFirst

---
+++ src/4.17.5/string/upperFirst.js
@@ -0,0 +1,10 @@
+const _ = require("lodash");
+
+/*
+ * 将第一个字符转换为大写；
+ * --------------------------------------------------
+ */
+/* Foo */
+console.log(_.upperFirst("foo"));
+/* FOO */
+console.log(_.upperFirst("FOO"));
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-require("./4.17.5/object/pick");
+require("./4.17.5/string/upperFirst");
