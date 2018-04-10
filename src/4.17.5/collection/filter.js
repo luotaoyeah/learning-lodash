1abff78a7730de2d35d3430d7f661978dcb9d33e
luotao <luotaoyeah@gmail.com>
2018-04-10 09:11:19 +0800

    misc

--- src/4.17.5/collection/filter.js
+++ src/4.17.5/collection/filter.js
@@ -1,20 +1,20 @@
 const _ = require("lodash");
 
 /*
- * 迭代列表元素，返回一个新的列表，新的列表中的元素对于断言回调返回值都为 true；
+ * 接收 2 个参数：'数组'，'断言'
+ * 迭代'数组'，返回一个新的数组，新的数组中的元素对于断言回调返回值都为 true；
  * --------------------------------------------------
  */
-
+console.log("----------: 基本用法");
 const array = [1, 2, 3, 4, 5];
-
 /* [ 1, 2 ] */
 console.log(_.filter(array, item => item < 3));
-console.log("----------");
 
 /*
  * 传递给断言回调函数 3 个参数：'当前元素'，'当前索引'，'列表'
  * --------------------------------------------------
  */
+console.log("----------: 回调参数");
 _.filter(array, (item, index, collection) => {
   console.log(item, index, collection);
   return true;
--- src/4.17.5/collection/forEachRight.js
+++ src/4.17.5/collection/forEachRight.js
@@ -1,10 +1,10 @@
 const _ = require("lodash");
 
-const array = [1, 2, 3, 4, 5];
-
 /*
- * 从右往左进行迭代
+ * 类似 _.forEach，从右往左进行迭代；
+ * --------------------------------------------------
  */
+const array = [1, 2, 3, 4, 5];
 /* 5, 4, 3, 2, 1 */
 _.forEachRight(array, item => {
   console.log(item);
